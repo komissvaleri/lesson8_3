66e2c952eae870c75bb7547dfa027c3e526ee1d8
komissvaleri <lera21734tkd@gmail.com>
2022-04-03 15:27:18 +0700

    add tasks

---
+++ task_3.py
@@ -0,0 +1,21 @@
+# 3. В массиве случайных целых чисел поменять местами
+# минимальный и максимальный элементы.
+
+import random
+
+r = [random.randint(0, 99) for _ in range(10)]
+print(f'Массив случайных чисел: {r}')
+
+max = r[0]
+min = r[0]
+
+for i in r:
+    if i > max:
+        max = i
+    elif i < min:
+        min = i
+min_index = r.index(min)
+max_index = r.index(max)
+r[min_index], r[max_index] = r[max_index], r[min_index]
+print(f'Массив после перестановки max и min значений {min_index} и {max_index}: {r}')
+
